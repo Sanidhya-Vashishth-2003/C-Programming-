4cf02333ecf19ac73edc418e8d7322a8507a73ea
Sanidhya Vashishth <146586891+Sanidhya-Vashishth-2003@users.noreply.github.com>
2023-11-28 00:29:00 +0530

    Add files via upload

---
+++ ASCII_Triangle_Pattern.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+
+int main()
+{
+int i,j,k,ascii=65,t,c;
+
+printf("enter range \n");
+scanf("%d",&t);
+
+for(i=1; i<=t; i++)
+{
+for(j=t; j>=i; j--)
+{
+printf("  ");
+}
+
+for(k=1; k<=i; k++)
+{
+
+if(ascii==91)
+ascii=97;
+
+
+if(ascii>122)
+ascii=178;
+printf("%2c",ascii++);
+}
+printf("\n");
+}
+return 0;
+}
